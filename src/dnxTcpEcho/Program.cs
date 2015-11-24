caa3be1ae961a7b35f50db10807c8a2f4ff4e6e2
YASUAKI MATSUDA <dark_crash@hotmail.com>
2015-11-24 22:41:45 +0900

    change runtime to rc1

--- src/dnxTcpEcho/Program.cs
+++ src/dnxTcpEcho/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Net.Sockets;
 
@@ -8,47 +9,65 @@ namespace dnxTcpEcho
     public class Program
     {
 
-        private Socket sock;
+        private static Socket server;
+        private static List<Socket> clientList = new List<Socket>();
+        private static object clientListLock = new object();
+        private static CancellationToken cancelToken = new CancellationToken();
 
-        public void Main(string[] args)
+        public static void Main(string[] args)
         {
-            this.InitSocket();
-            try
+            InitSocket();
+            while (true)
+            {
+                var inp = Console.ReadKey(true);
+                if (inp.Key == ConsoleKey.Escape) break;
+                var data = System.Text.Encoding.ASCII.GetBytes( inp.KeyChar.ToString());
+                var result = Parallel.ForEach(clientList, async (client) => await SocketTaskExtensions.SendAsync(client, new ArraySegment<byte>(data), SocketFlags.None));
+            }
+        }
+
+
+        private static void InitSocket()
+        {
+            server = new Socket(SocketType.Stream, ProtocolType.Tcp);
+            var endp = new System.Net.IPEndPoint(System.Net.IPAddress.Any, 5999);
+            server.Bind(endp);
+            server.Listen(100);
+            Console.WriteLine($"{nameof(InitSocket)} {endp}");
+
+            Action loopAccept = async () =>
             {
                 while (true)
                 {
-                    var client = sock.Accept();
-                    var t = new Task(() => this.InitSocketClient(client), TaskCreationOptions.LongRunning);
-                    t.Start(TaskScheduler.Default);
+                    var client = await SocketTaskExtensions.AcceptAsync(server);
+                    var t = new Task(_ => InitSocketClient(client), cancelToken, TaskCreationOptions.LongRunning);
+                    t.Start();
                 }
-            }
-            finally
-            {
-                sock.Dispose();
-            }
+            };
+            var loopAcceptTask = new Task(loopAccept, cancelToken, TaskCreationOptions.LongRunning);
+            loopAcceptTask.Start();
+
+
         }
 
 
-        private void InitSocket()
+        private static async void InitSocketClient(Socket client)
         {
-            this.sock = new Socket(SocketType.Stream, ProtocolType.Tcp);
-            var endp = new System.Net.IPEndPoint(System.Net.IPAddress.Any, 5999);
-            sock.Bind(endp);
-            sock.Listen(100);
-        }
-
-
-        private void InitSocketClient(Socket client)
-        {
+            lock (clientListLock)
+                clientList.Add(client);
+            var endp = client.RemoteEndPoint.ToString();
+            Console.WriteLine($"Accept Client {endp}");
             byte[] buf = new byte[1024];
 
             while (true)
             {
-                var size = client.Receive(buf);
+                var size = await SocketTaskExtensions.ReceiveAsync(client, new ArraySegment<byte>(buf), SocketFlags.None);
                 if (size == 0) break;
-                client.Send(buf, size, SocketFlags.None);
+                await SocketTaskExtensions.SendAsync(client, new ArraySegment<byte>(buf, 0, size), SocketFlags.None);
             }
-
+            lock (clientListLock)
+                clientList.Remove(client);
+            Console.WriteLine($"Close Client {endp}");
         }
 
     }
