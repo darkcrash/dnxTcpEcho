46d4b3e576b449792efd5cb93a5c6353160162be
YASUAKI MATSUDA <dark_crash@hotmail.com>
2015-11-29 23:12:35 +0900

    change docker.sh.     remove git clone.

--- docker.sh
+++ docker.sh
@@ -1,14 +1,5 @@
 #!/bin/bash
 
-cd
-
-# clean up clone
-rm -Rf dnxTcpEcho
-
-# git clone
-git clone https://github.com/darkcrash/dnxTcpEcho.git
-cd ~/dnxTcpEcho/
-
 # Docker stop
 #DOCKERCONTAINARID=`docker ps -f image=dnxtcpecho --no-trunc=false -q`
 #docker stop $DOCKERCONTAINARID
